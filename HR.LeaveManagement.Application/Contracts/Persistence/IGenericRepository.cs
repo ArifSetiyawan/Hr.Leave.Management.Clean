306129d71133bba17c8c6b083e36679acc45fb17
Muhammad Arif <muhamadarief251@gmail.com>
2023-09-25 16:48:13 +0700

    first-training

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<List<T>> GetAsync();
+
+    Task<T> GetByIdAsync(int id);
+
+    Task<T> CreateAsync(T entitiy);
+
+    Task<T> UpdateAsync(T entitiy);
+
+    Task<T> DeleteAsync(T entitiy);
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // Validate Incoming Data
+            var validator = new UpdateLeaveTypeCommandValidator(_leaveTypeRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+                throw new BadRequestExceptions("Invalid Leave Type", validationResult);
+
+            // Convert to Domain Entity Object
+            var leaveTypeUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            // Add to Database
+            await _leaveTypeRepository.UpdateAsync(leaveTypeUpdate);
+
+            // Return Record ID
+            return Unit.Value;
+        }
+    }
+}
